ef9edbf96fdc694a32d509e89749c367c8b2fe8e
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-15 14:59:47 -0400

    Update DisplayPosts.jsx

--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -3,5 +3,31 @@ import './DisplayPosts.css';
 import UserPost from '../UserPost';
 
 
+const DisplayPosts = (props) => {
+  return (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Post</th>
+      </tr>
+    </thead>
+    <tbody>
+    {entries.map((entry) => {
+      return (
+        <tr>
+          <td>{entry.name}</td>
+          <td>{entry.post}</td>
+        </tr>
+      );
+
+    })}
+    </tbody>
+  </table>
+  );
+
+
+}
+export default DisplayPosts;
 
 
