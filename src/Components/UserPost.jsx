e411a2bb06c15742ff8f333dc5fdb2427021e668
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-15 16:37:44 -0400

    Update UserPost.jsx

--- src/Components/UserPost.jsx
+++ src/Components/UserPost.jsx
@@ -21,27 +21,18 @@ const UserPost = (props) => {
     // we are sending name and post to an API
     //Clear the form fields after submission
     
-  };
+  }
   return (
     <form className='user-post' onSubmit ={handleSubmit}>
       <label>Name:</label>
-      <input 
-          type ='text'
-          id = 'name'
-          value ={name}
-          onChange={(e) => setName(e.target.value)}
-          />
+      <input type ='text' value ={name} onChange={(e) => setName(e.target.value)} />
       <label>Post:</label>
-        <input
-           id='post'
-           value={post}
-           onChange={(e) => setPost(e.target.value)}
-        />
+        <input type='text' value={post} onChange={(e) => setPost(e.target.value)} />
       <button className = 'btn btn-primary' button type='submit' style={{ width: "60px", height: "30px", font:"small-caption"}}>Create</button>
     </form>
 
-    );
-  };
+  );
+};
 
 
 
